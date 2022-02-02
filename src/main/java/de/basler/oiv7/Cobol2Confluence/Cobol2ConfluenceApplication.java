3b00552bab61d65cde2cc17c3f755a09a356292e
codewithtimmy <timmyscode@web.de>
2022-02-02 10:37:30 +0100

    First Commit -- new Project

---
+++ src/main/java/de/basler/oiv7/Cobol2Confluence/Cobol2ConfluenceApplication.java
@@ -0,0 +1,14 @@
+package de.basler.oiv7.Cobol2Confluence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cobol2ConfluenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cobol2ConfluenceApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/de/basler/oiv7/Cobol2Confluence/Cobol2ConfluenceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.basler.oiv7.Cobol2Confluence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cobol2ConfluenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
